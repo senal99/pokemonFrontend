d8248e77260879a39a249cc2bfc413afb1f8110a
senal99 <senal.weerasekara@insighture.com>
2023-07-25 17:45:48 +0530

    docker added

--- pokemonFrontend/src/main.jsx
+++ pokemonFrontend/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import RouterFile from './routes/route.jsx'
+import ReactDOM from "react-dom";
 import { Provider } from 'react-redux'
 import store from './redux/store.jsx'
 
 //StickMode is disabled cus it makes the components render two times. i think.
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   // <React.StrictMode>
   <BrowserRouter>
     <Provider store={store}>
@@ -14,4 +14,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </BrowserRouter>
   // </React.StrictMode> 
-)
+  , document.getElementById('root'));
